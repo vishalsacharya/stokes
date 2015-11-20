edcb24d9288da787fbce06d4a70616b6c4630bde
Arash Bakhtiari <bakhtiar@in.tum.de>
2015-11-20 10:12:09 -0600

    FIX: compilation problem with the latest TbSLAS

--- src/stokes.cpp
+++ src/stokes.cpp
@@ -30,6 +30,7 @@ static char help[] = "\n\
 
 // TBSLAS INCLUDES
 #include <utils/common.h>
+#include <utils/fields.h>
 #include <tree/utils_tree.h>
 #include <tree/semilag_tree.h>
 #include <utils.hpp>
@@ -87,6 +88,54 @@ double tcurr = 0;
 
 //PetscReal  L                = 500;
 std::vector<double>  pt_coord;
+
+template<typename real_t, int sdim>
+void
+get_gaussian_field_cylinder_atT(const real_t* points_pos,
+                                int num_points,
+                                real_t* out) {
+  real_t xc      = 0.6;
+  real_t yc      = 0.5;
+  real_t r = sqrt((xc-0.5)*(xc-0.5) + (yc-0.5)*(yc-0.5));
+  xc = 0.5+r*cos(tcurr);
+  yc = 0.5+r*sin(tcurr);
+  const real_t theta   = 0.0;
+  const real_t sigma_x = 0.06;
+  const real_t sigma_y = 0.06;
+  const real_t A       = 1.0;
+
+  tbslas::get_gaussian_field_cylinder<real_t, sdim>(points_pos,
+                                                    num_points,
+                                                    out,
+                                                    xc,
+                                                    yc,
+                                                    theta,
+                                                    sigma_x,
+                                                    sigma_y,
+                                                    A);
+}
+
+template<typename real_t, int sdim>
+void
+get_slotted_cylinder_atT(const real_t* points_pos,
+                         int num_points,
+                         real_t* out) {
+  real_t xc = 0.5;
+  real_t yc = 0.5;
+  real_t zc = 0.5;
+  real_t R  = 0.3;
+  real_t w  = 0.1;
+  real_t a  = tcurr;
+  tbslas::get_slotted_cylinder<real_t, sdim>(points_pos,
+                                             num_points,
+                                             out,
+                                             xc,
+                                             yc,
+                                             zc,
+                                             R,
+                                             w,
+                                             a);
+}
 
 template<typename real_t, int sdim>
 void
